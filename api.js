b1b9699ae16cda58f8ac554949f59a41866bb65d
john <jxh6494@rit.edu>
2018-11-16 17:44:23 -0500

    removed frontend folder

--- api.js
+++ api.js
@@ -1,3 +1,7 @@
+
+const request = require('request');
+
+
 module.exports = {
   getItemLocation: function () {
     // getItemLocation impl
@@ -9,7 +13,11 @@ module.exports = {
     // getPrice impl
   },
   getRecipe: function () {
-    // getRecipe impl
+    request('https://api.edamam.com/search?q=chicken&app_id=1f5e9cb1&app_key=94433d21b98f8e44def6c39271734749&from=0&to=3&calories=591-722&health=alcohol-free', { json: true }, (err, res, body) => {
+      if (err) { return console.log(err); }
+      console.log(body.url);
+      console.log(body.explanation);
+    });
   },
   getAllRecipes: function () {
     // getAllRecipes impl
